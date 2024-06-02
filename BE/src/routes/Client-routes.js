b8178d9fba13d52f50cf5179b76ac2ea92c63199
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-02 17:58:43 +0300

    Adaugare ruta get Client dupa clientId

--- BE/src/routes/Client-routes.js
+++ BE/src/routes/Client-routes.js
@@ -3,20 +3,19 @@ import requireAuth from "../middlewares/require-auth.js";
 import RoleAuthorization from "../models/role-auhorization.js";
 import express from "express";
 import Client from "../models/Client.js";
+import Profile from "../models/Profile.js";
+import profileRouter from "./Profile-routes.js";
 
 const clientRouter = express.Router();
 clientRouter.post("/connect/:coachId", currentUser, requireAuth, async (req, res) => {
     try {
         const clientId = req.currentUser.id;
-        const { coachId } = req.params;
+        const {coachId} = req.params;
 
         const role = new RoleAuthorization(req.currentUser.roles);
         if (role.name !== "CLIENT") {
-            return res.status(403).json({ message: "Access denied" });
+            return res.status(403).json({message: "Access denied"});
         }
-
-        console.log(clientId);
-        console.log(coachId);
 
         const newClientData = {
             clientId,
@@ -28,9 +27,29 @@ clientRouter.post("/connect/:coachId", currentUser, requireAuth, async (req, res
         const newClient = new Client(newClientData);
         await newClient.save();
 
-        return res.status(200).json({ newClient });
+        return res.status(200).json({newClient});
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message: error.message});
+    }
+});
+
+clientRouter.get('/application/client/:clientId', currentUser, requireAuth, async (req, res) => {
+    const clientId = req.currentUser.id;
+
+    try {
+        const role = new RoleAuthorization(req.currentUser.roles);
+        if (role.name !== "CLIENT") {
+            return res.status(403).json({ message: "Access denied" });
+        }
+        const client = await Client.findOne({clientId: clientId});
+
+        if (!client) {
+            return res.status(404).json({message: 'Client not found'});
+        }
+        res.json(client);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server error', error: error.message});
     }
 });
 export default clientRouter;
