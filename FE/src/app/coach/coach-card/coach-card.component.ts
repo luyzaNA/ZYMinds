11da43cd4e9ccf14e5292bb26121bc709560db91
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-02 15:19:08 +0300

    Adaugare buton  de Apply pe cardul de antrenor

--- FE/src/app/coach/coach-card/coach-card.component.ts
+++ FE/src/app/coach/coach-card/coach-card.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {User, UserInformation} from "../../shared/user";
 import {ProfileService} from "../../services/profile.service";
 import {AuthService} from "../../services/auth.service";
-import {ProfileI} from "../../shared/Profile";
 
 @Component({
   selector: 'app-coach-card',
@@ -12,11 +11,12 @@ import {ProfileI} from "../../shared/Profile";
 export class CoachCardComponent {
   currentUser!: User;
   @Input() userProfileInformation!: UserInformation;
-  constructor(protected profileService: ProfileService, protected authService: AuthService) {
 
-this.currentUser = this.authService.getCurrentUser();
+  constructor(protected profileService: ProfileService, protected authService: AuthService) {
+    this.currentUser = this.authService.getCurrentUser();
   }
 
-
-
+  applApplForCoaching() {
+    console.log("Hei");
+  }
 }
