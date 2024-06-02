1176604922e9f2ae3b5a364b3ada2154fc90e318
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-02 16:07:27 +0300

    Adaugare sectiune de mesaj pentru client

--- FE/src/app/shared/Client/ClientI.ts
+++ FE/src/app/shared/Client/ClientI.ts
@@ -2,16 +2,19 @@ export interface ClientI{
   userId: string;
   coachId: string;
   status: 'pending' | 'approved' | 'rejected';
+  message: string;
 }
 
 export class Client implements ClientI {
   userId: string;
   coachId: string;
   status: 'pending' | 'approved' | 'rejected';
+  message:string;
 
   constructor() {
     this.userId = '';
     this.coachId = '';
     this.status = 'pending';
+    this.message = '';
   }
 }
