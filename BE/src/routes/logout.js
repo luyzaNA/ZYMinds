4250c82df936685ba37d436e0b1efd32e28b7489
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-20 11:27:59 +0300

    Editare interfata FE

--- BE/src/routes/logout.js
+++ BE/src/routes/logout.js
@@ -1,9 +1,11 @@
 import express from "express";
+import currentUser from "../middlewares/current-user.js";
+import requireAuth from "../middlewares/require-auth.js";
 
 const logOutRouter = express.Router();
 
-logOutRouter.post('/logout', (req, res) => {
-    res.send({ logout: true });
+logOutRouter.post('/logout', currentUser, requireAuth, (req, res) => {
+    res.send({logout: true});
 });
 
 export default logOutRouter;
