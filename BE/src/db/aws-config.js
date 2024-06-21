e1b138ade47a4701177b17ed27ad6bd28a8302b8
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-21 09:49:29 +0300

    fixed pages styling responsiveness and refactor login register

--- BE/src/db/aws-config.js
+++ BE/src/db/aws-config.js
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk';
 AWS.config.update({
     accessKeyId: 'AKIA6GBMB22IMHJ5T5HV',
-    secretAccessKey:  '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'
+    secretAccessKey:  process.env.AWS_SECRET
 });
 
 export default AWS;
